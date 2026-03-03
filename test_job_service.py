f2c5563b8a8be9f8bec29633aaa6f1c6c1b62159
SarthakCham <109859379+SarthakCham@users.noreply.github.com>
2026-03-03 13:22:49 -0500

    Delete test_job_service.py

--- test_job_service.py
+++
@@ -1,6 +0,0 @@
-from job_service import create_job
-
-def test_create_job_sets_status_to_queued():
-    job = create_job("submission.zip")
-    assert job["status"] == "queued"
-    assert job["original_filename"] == "submission.zip"
