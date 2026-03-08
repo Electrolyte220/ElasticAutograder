1259d9918c16f58d0c54ee7db84a744632cf492e
Electrolyte220 <cjm03c@gmail.com>
2026-03-08 14:52:56 -0400

    Create json objects for uploaded files, add gson config for returning json objects from api

--- backend/src/main/java/com/autograder/controller/JobController.java
+++ backend/src/main/java/com/autograder/controller/JobController.java
@@ -7,15 +7,13 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import com.autograder.model.Job;
 import com.autograder.repository.JobRepository;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.print.DocFlavor.INPUT_STREAM;
 import java.util.List;
+import java.util.Optional;
 
 // this must be changed in prod
 @CrossOrigin(origins = "http://localhost:5173")
@@ -29,28 +27,21 @@ public class JobController {
         this.jobRepository = jobRepository;
     }
     @PostMapping({"/jobs/upload"})
-    public ResponseEntity<String> createJob(@RequestParam MultipartFile file) {
+    public ResponseEntity<String> uploadFile(@RequestParam MultipartFile file) {
         //create jobs & upload
         String fileName = file.getOriginalFilename();
         try {
             byte[] bytes = file.getBytes();
-            Path path = Path.of("uploads/" + fileName);
-            if(!Files.exists(path)) {
-                Files.createDirectories(Path.of("uploads"));
+            Path uploads = Path.of("uploads");
+            Path filePath = Path.of(uploads +  "/" + fileName);
+            if(!Files.exists(uploads)) {
+                Files.createDirectories(uploads);
             }
-            Files.write(path, bytes);
+            Files.write(filePath, bytes);
             return ResponseEntity.ok("Successfully uploaded file.");
         } catch(IOException e) {
             return ResponseEntity.status(500).body("Failed to read file: " + fileName);
         }
-    }
-
-    @GetMapping("/jobs")
-    public ResponseEntity<JsonObject> getAllJobs() {
-        //job history & filters
-        JsonObject test = new JsonObject();
-        test.addProperty("test", "If you see this in your browser, it works!");
-        return ResponseEntity.ok(test);
     }
 
     // added by app0cal
@@ -60,13 +51,42 @@ public class JobController {
     }
 
     @GetMapping("/job/{id}")
-    public ResponseEntity<JsonObject> getJobObject(@PathVariable String id) {
+    public ResponseEntity<JsonObject> getJobObject(@PathVariable Long id) {
         //job details & result
-        JsonObject exampleJob = new JsonObject();
-        exampleJob.addProperty("job_id", id);
-        exampleJob.addProperty("assignment_id", "123");
-        exampleJob.addProperty("createdAt", LocalDateTime.now().toString());
-        return ResponseEntity.ok(exampleJob);
+        JsonObject jobObj = new JsonObject();
+        Optional<Job> jobEntity = jobRepository.findById(id);
+        if(jobEntity.isEmpty()) {
+            jobObj.addProperty("Error", "No job with this id exists.");
+            return ResponseEntity.status(404).body(jobObj);
+        }
+        Job job = jobEntity.get();
+        jobObj.addProperty("jobId", job.getId());
+        jobObj.addProperty("assignmentId", job.getAssignmentId());
+        jobObj.addProperty("originalFileName", job.getOriginalFilename());
+        jobObj.addProperty("submissionPath", job.getSubmissionPath());
+        jobObj.addProperty("graderImage", job.getGraderImage());
+        jobObj.addProperty("status", job.getStatus().toString());
+        jobObj.addProperty("createdAt", job.getCreatedAt().toString());
+        jobObj.addProperty("updatedAt", job.getUpdatedAt().toString());
+
+        if(job.getStartedAt() != null) {
+            jobObj.addProperty("startedAt", job.getStartedAt().toString());
+        } else {
+            jobObj.addProperty("startedAt", (String) null);
+        }
+        if(job.getFinishedAt() != null) {
+            jobObj.addProperty("finishedAt", job.getFinishedAt().toString());
+        } else {
+            jobObj.addProperty("finishedAt", (String) null);
+        }
+
+        jobObj.addProperty("score", job.getScore());
+        jobObj.addProperty("testsPassed", job.getTestsPassed());
+        jobObj.addProperty("testsTotal", job.getTestsTotal());
+        jobObj.addProperty("errorMessage", job.getErrorMessage());
+        jobObj.addProperty("resultJson", job.getResultJson());
+        jobObj.addProperty("jobName", job.getK8sJobName());
+        return ResponseEntity.ok(jobObj);
     }
 
     @PostMapping("/job/{id}/callback")
