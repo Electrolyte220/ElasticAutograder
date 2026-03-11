2c39c4c2b83a1066cb472cea6f537a8678a02344
Electrolyte220 <cjm03c@gmail.com>
2026-03-11 17:59:41 -0400

    let's go even simpler

--- backend/grading/graders/main.py
+++ backend/grading/graders/main.py
@@ -178,8 +178,8 @@ def main():
     )
 
     test_cases = [
-        {"name": "case_1", "args": [1]},
-        {"name": "case_2", "args": [3]},
+        {"name": "case_1", "args": [1, 4]},
+        {"name": "case_2", "args": [3, 2]},
     ]
 
     tests_passed = 0
