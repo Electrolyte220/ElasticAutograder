d29c7693610d9d80fe67e044006c541f0f69ad01
Electrolyte220 <cjm03c@gmail.com>
2026-04-27 22:47:09 -0400

    update links

--- frontend/src/api/jobs.js
+++ frontend/src/api/jobs.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8080/api";
+const API_BASE = "https://ea.electrolyte.dev/api";
 
 export async function fetchRecentJobs() {
   const res = await fetch(`${API_BASE}/jobs/recent`);
